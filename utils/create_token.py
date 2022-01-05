95e8378039c674e0e0993d05eef98c5b7c11cfdd
Dylan Stocking <40648525+PhantomLeak@users.noreply.github.com>
2022-01-05 17:02:56 -0500

    Update security for secret inputs

--- utils/create_token.py
+++ utils/create_token.py
@@ -1,8 +1,9 @@
+import stdiomask
 def create_token():
     creds = {}
-    creds["client_id"] = input("Client_id: ")
-    creds["client_secret"] = input("client_secret: ")
-    creds["user_agent"] = input("user_agent: ")
-    creds["username"] = input("username: ")
-    creds["password"] = input("password: ")
+    creds['client_id'] = input('client_id: ')
+    creds['client_secret'] = stdiomask.getpass(prompt="client_secret: ")
+    creds['user_agent'] = input('user_agent: ')
+    creds['username'] = input('username')
+    creds['password'] = stdiomask.getpass()
     return creds
